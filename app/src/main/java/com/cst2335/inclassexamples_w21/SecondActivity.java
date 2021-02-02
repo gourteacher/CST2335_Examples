a9f80815c31af36f1adf51d46dd9d44e337cb07c
Samira Ouaaz <souaaz@yahoo.com>
2021-02-01 22:45:50 -0500

    Clean up layout files

--- app/src/main/java/com/cst2335/inclassexamples_w21/SecondActivity.java
+++ app/src/main/java/com/cst2335/inclassexamples_w21/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,11 +13,30 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button previousButton = findViewById(R.id.previousPageButton);
+        Intent dataFromPreviousPage = getIntent();
+        String nameFromPage1 = dataFromPreviousPage.getStringExtra("name");
+        int ageFromPage1 = dataFromPreviousPage.getIntExtra("age", 0);
+        String whatUserTyped = dataFromPreviousPage.getStringExtra("typed");
 
-        previousButton.setOnClickListener(click ->
-                finish()
-        );
+        TextView age = findViewById(R.id.ageField);
+        age.setText("Age is:" + ageFromPage1);
 
+        TextView name = findViewById(R.id.nameField);
+        name.setText("Name is:" + nameFromPage1);
+
+        TextView typed = findViewById(R.id.typedField);
+        typed.setText( "You typed in page 1:"+ whatUserTyped );
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener( clk ->
+        {
+            setResult(50, null); //a unique number that can only be 50 if you clicked on the back button
+            finish();
+        });
+
+        Button save = findViewById(R.id.saveButton);
+        save.setOnClickListener( bt -> {
+            startActivity(new Intent(SecondActivity.this, SharedPreferencesExample.class));
+        });
     }
 }
