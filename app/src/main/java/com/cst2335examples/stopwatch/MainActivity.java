aa3a8e245b87869d6a6c56c48de77bdab08b4394
Samira Ouaaz <souaaz@yahoo.com>
2021-02-03 10:37:22 -0500

    Stop Watch Example - basic version

---
+++ app/src/main/java/com/cst2335examples/stopwatch/MainActivity.java
@@ -0,0 +1,91 @@
+package com.cst2335examples.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.os.SystemClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String CAT = "MainActivity";
+
+    Boolean isRunning = false;
+    long offset = 0;
+    Chronometer stopwatch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println( CAT + "onCreate()");
+
+        setContentView(R.layout.activity_main);
+
+        stopwatch =  findViewById(R.id.stopwatch);
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener ( clk ->  {
+            if (!isRunning) {
+                setBaseTime();
+                stopwatch.start();
+                isRunning = true;
+            }
+        });
+
+        Button stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener ( clk ->  {
+            if (isRunning) {
+                saveOffset();
+                stopwatch.stop();
+                isRunning = false;
+            }
+        });
+
+        Button resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener ( clk ->  {
+            offset = 0;
+            setBaseTime();
+        });
+    }
+
+
+
+    void setBaseTime() {
+        stopwatch.setBase(SystemClock.elapsedRealtime() - offset);
+    }
+
+    void saveOffset() {
+        offset = SystemClock.elapsedRealtime() - stopwatch.getBase();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println( CAT + "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println( CAT + "onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println( CAT + "onPause()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println( CAT + "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println( CAT + "onDestroy()");
+    }
+}
