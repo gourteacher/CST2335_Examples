82e628acf8537b1b14f161ba3812d47d9d76c986
Samira Ouaaz <souaaz@yahoo.com>
2021-02-03 12:19:21 -0500

    Stop Watch Example - Change the log tag

--- app/src/main/java/com/cst2335examples/stopwatch/MainActivity.java
+++ app/src/main/java/com/cst2335examples/stopwatch/MainActivity.java
@@ -10,7 +10,7 @@ import android.os.SystemClock;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String CAT = "MainActivity";
+    final String TAG = "MainActivity";
 
     Boolean isRunning = false;
     long offset = 0;
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println( CAT + "onCreate()");
+        System.out.println( TAG + "onCreate()");
 
         setContentView(R.layout.activity_main);
 
@@ -62,30 +62,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        System.out.println( CAT + "onStart()");
+        System.out.println( TAG + "onStart()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        System.out.println( CAT + "onStop()");
+        System.out.println( TAG + "onStop()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        System.out.println( CAT + "onPause()");
+        System.out.println( TAG + "onPause()");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        System.out.println( CAT + "onRestart");
+        System.out.println( TAG + "onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        System.out.println( CAT + "onDestroy()");
+        System.out.println( TAG + "onDestroy()");
     }
 }
