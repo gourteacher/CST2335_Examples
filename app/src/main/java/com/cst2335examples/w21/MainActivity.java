18a535503cca76a422c1a32825098133686303ae
Samira Ouaaz <souaaz@yahoo.com>
2021-03-11 23:54:26 -0500

    Added JSON processing in UrlFetcher

--- app/src/main/java/com/cst2335examples/w21/MainActivity.java
+++ app/src/main/java/com/cst2335examples/w21/MainActivity.java
@@ -1,87 +1,90 @@
 package com.cst2335examples.w21;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "MainActivity";
+
+    EditText search;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyHTTPRequest req = new MyHTTPRequest();
-        req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
+        search = (EditText)findViewById(R.id.searchBar);
+        result = (TextView) findViewById(R.id.result);
+
+        Button addButton = findViewById(R.id.fetchItem);
+        addButton.setOnClickListener( click -> {
+            String currency = search.getText().toString();
+            String queryURL = "https://api.exchangeratesapi.io/latest?base=USD";
+            String[] myTaskParams = { queryURL, currency };
+            MyNetworkQuery theQuery = new MyNetworkQuery();
+            theQuery.execute(myTaskParams );
+        });
     }
 
+    //                                      Type1, Type2   Type3
+    private class MyNetworkQuery extends AsyncTask<String, String, String>{
 
-    //Type1     Type2   Type3
-    private class MyHTTPRequest extends AsyncTask< String, Integer, String>
-    {
-        //Type3                Type1
-        public String doInBackground(String ... args)
-        {
+        @Override                       //Type 1
+        protected String doInBackground(String ... strings) {
+            String res = null;
+
+            //default values
+            String cur = "CAD";
+            String queryURL=  "https://api.exchangeratesapi.io/latest?base=USD";
+
+            if (strings.length > 1 && strings[1].length() > 2) {
+                cur = strings[1];
+            }
+
+            if (strings.length > 0 && strings[0].length() > 16) {
+                queryURL = strings[0];
+            }
+
+
             try {
 
-                //create a URL object of what server to contact:
-                URL url = new URL(args[0]);
+                UrlFetcher url = new UrlFetcher();
+                String rate = url.fetchItems(queryURL, cur);
 
-                //open the connection
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                //wait for data:
-                InputStream response = urlConnection.getInputStream();
-
-                //JSON reading:   Look at slide 26
-                //Build the entire string response:
-                BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
-                StringBuilder sb = new StringBuilder();
-
-                String line = null;
-                while ((line = reader.readLine()) != null)
-                {
-                    sb.append(line + "\n");
-                }
-                String result = sb.toString(); //result is the whole string
-
-
-                // convert string to JSON: Look at slide 27:
-                JSONObject uvReport = new JSONObject(result);
-
-                //get the double associated with "value"
-                double uvRating = uvReport.getDouble("value");
-
-                Log.i("MainActivity", "The uv is now: " + uvRating) ;
-
+                res =  "Rate for " + cur + " : " + rate ;
             }
             catch (Exception e)
             {
-
+                res = "Failed to fetch items from URL";
             }
-
-            return "Done";
+            //What is returned here will be passed as a parameter to onPostExecute:
+            return res;
         }
 
-        //Type 2
-        public void onProgressUpdate(Integer ... args)
-        {
-
+        @Override                   //Type 3
+        protected void onPostExecute(String sentFromDoInBackground) {
+            super.onPostExecute("Finished background thread");
+            //update GUI Stuff:
+            if (sentFromDoInBackground != null ) {
+                result.setText(sentFromDoInBackground);
+            } else {
+                result.setText("Finished background thread with an unknown error");
+            }
         }
-        //Type3
-        public void onPostExecute(String fromDoInBackground)
-        {
-            Log.i("HTTP", fromDoInBackground);
+
+        @Override                       //Type 2
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
         }
     }
 }
-
