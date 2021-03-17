cf17147a639451dfa6dc79d734b729a30c9dff93
Samira Ouaaz <souaaz@yahoo.com>
2021-03-17 13:58:05 -0400

    Week 11 - Toolbar example

---
+++ app/src/main/java/com/cst2335/inclassexamples_w21/MenuExample.java
@@ -0,0 +1,63 @@
+package com.cst2335.inclassexamples_w21;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MenuExample extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_menu_example);
+
+            //This gets the toolbar from the layout:
+            Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
+
+            //This loads the toolbar, which calls onCreateOptionsMenu below:
+            setSupportActionBar(tBar);
+
+        }
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu items for use in the action bar
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.example_menu, menu);
+
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            String message = null;
+            //Look at your menu XML file. Put a case for every id in that file:
+            switch(item.getItemId())
+            {
+                //what to do when the menu item is selected:
+                case R.id.item1:
+                    message = "You clicked item 1";
+                    break;
+                case R.id.search_item:
+                    message = "You clicked on the search";
+                    break;
+                case R.id.help_item:
+                    message = "You clicked on help";
+                    break;
+                case R.id.mail:
+                    message = "You clicked on mail";
+                    break;
+            }
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+    }
+
+
